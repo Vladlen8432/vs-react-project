c9a7df57adaa1fad0bef709f012f8149e6aecd0c
Vladlen8432 <127045075+Vladlen8432@users.noreply.github.com>
2024-10-15 16:29:03 +0300

    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,40 @@
+import { productsData } from 'productsData';
+
 export const App = () => {
   return (
     <div>
       <h1>Picture store</h1>
-      <Product name="Mountains" price="10.99" />
-      <Product name="Ocean" price="8.49" />
-      <Product name="Lake" price="6.80" />
+      <Product title="Mountains" price="10.99" discount={null} />
+      <Product title="Ocean" price="8.49" discount={1.5} />
+      <Product title="Lake" price="6.80" discount={2.33} />
     </div>
   );
 };
 
-const Product = ({ name, price }) => {
+const Product = ({ title, price, discount }) => {
   return (
-    <div>
-      <img
-        src="https://i.pinimg.com/564x/09/74/d3/0974d3430c55bef1c076466e295a90d0.jpg"
-        alt="Tacos With Lime"
-        width="640"
-      />
-      <h2>{name}</h2>
-      <p>Price: {price}$</p>
-      <button type="button">Add to cart</button>
-    </div>
+    // <div>
+    //   <img
+    //     src="https://i.pinimg.com/564x/09/74/d3/0974d3430c55bef1c076466e295a90d0.jpg"
+    //     alt="Tacos With Lime"
+    //     width="640"
+    //   />
+    //   <h2>{title}</h2>
+    //   {discount && <h3>Discount: {discount}$</h3>}
+    //   <p>Price: {price}$</p>
+    //   <button type="button">Add to cart</button>
+    // </div>
+    <section>
+      {productsData.map(product => {
+        return (
+          <Product
+            key={product.id}
+            title={product.title}
+            price={product.price}
+            discount={product.discount}
+          />
+        );
+      })}
+    </section>
   );
 };
---
+++ src/productsData.js
@@ -0,0 +1,20 @@
+export const productsData = [
+  {
+    id: '1',
+    title: 'Mountains',
+    price: '10.99',
+    discount: null,
+  },
+  {
+    id: '2',
+    title: 'Ocean',
+    price: '8.49',
+    discount: 1.5,
+  },
+  {
+    id: '3',
+    title: 'Lake',
+    price: '6.80',
+    discount: 2.33,
+  },
+];
